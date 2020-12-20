a6daf0d0b714629d27580c3b60500ab3c9a6eb15
alyahyaoui <ali.codeur@gmail.com>
2020-12-21 00:17:34 +0100

    just an update to test 2

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 server.applyMiddleware({ app });
 
